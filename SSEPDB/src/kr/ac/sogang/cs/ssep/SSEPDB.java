747a78c178f00e3c0f87db17357c3b5aca9ace6a
minhoryang <minhoryang@gmail.com>
2014-06-20 14:18:26 +0900

    Fix Issue #1

---
+++ SSEPDB/src/kr/ac/sogang/cs/ssep/SSEPDB.java
@@ -0,0 +1,11 @@
+package kr.ac.sogang.cs.ssep;
+
+public class SSEPDB {
+
+	public static String myS = "hello";
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
