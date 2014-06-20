04745b4f01f9c74584bb2d09a13f29d21ec625e8
minhoryang <minhoryang@gmail.com>
2014-06-20 14:06:36 +0900

    Create New Android App Project.

---
+++ SSEP/src/kr/ac/sogang/cs/ssep/MainActivity.java
@@ -0,0 +1,13 @@
+package kr.ac.sogang.cs.ssep;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.mainactivity);
+	}
+}
